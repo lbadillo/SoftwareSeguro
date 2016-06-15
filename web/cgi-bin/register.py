c1304f63215248b40a9c43d600903b3ff77deca0
Samurai <samurai@samurai-wtf.localhost>
2016-06-16 00:35:47 +0200

    LBD proyecto inicial

---
+++ web/cgi-bin/register.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+import MySQLdb
+import cgi, cgitb
+cgitb.enable()
+
+def manageError(cod, message):
+  resp=message
+  if cod == 1062:
+    resp = "Este correo electronico ya fue registrado previamente"
+  return resp
+
+form = cgi.FieldStorage()
+
+first_name=form.getvalue('first_name')
+last_name=form.getvalue('last_name')
+e_mail=form.getvalue('e_mail')
+password=form.getvalue('password')
+amount=form.getvalue('amount')
+
+db = MySQLdb.connect("localhost","root","cristi79","banksafe")
+
+cursor = db.cursor()
+
+
+
+
+sql = "insert into client (name,lastName,email,password, amount,state) values ('%s','%s','%s','%s','%s','%d') " % (first_name,last_name,e_mail,password,amount,0)
+
+try:
+  cursor.execute(sql)
+  db.commit()
+  respcod = 0
+  respon="Felicidades!!!!  Despues de efectuar el proceso de verificacion sera enviado un correo de bienvenida a nuestro banco"
+except MySQLdb.Error, e:
+  db.rollback()
+  respon= manageError(e.args[0], e.args[1])
+  respcod=e.args[0]
+
+
+
+  
+print "Content-type: text/html\r\n\r\n";
+print '<html>'
+print '<head>'
+print '<title>Registro Cliente</title>'
+print '</head>'
+print '<body>'
+print '<h2> %s</h2>' % (respon)
+if respcod == 0:
+   print "<a href = '../index.html'> Volver a login</a>"
+ 
+else:
+   print "<a href = '../register.html'> Volver a registro</a>"
+  
+ 
+  
+print '</body>'
+print '</html>'
+
+db.close()
